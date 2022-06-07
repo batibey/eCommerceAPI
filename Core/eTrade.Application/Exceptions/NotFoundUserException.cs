c07cc9b603459119a74adb1a6001aa9495d6ae13
Mustafa BATI <mustafa.bati9@gmail.com>
2022-06-07 23:18:36 +0300

    SignIn

---
+++ Core/eTrade.Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTrade.Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() : base("Kullanıcı Adı veya Şifre Hatalı")
+        {
+
+        }
+
+        public NotFoundUserException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundUserException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Core/eTrade.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTrade.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandResponse
+    {
+
+    }
+}
