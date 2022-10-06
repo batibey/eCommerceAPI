98d51d9597bba39b87d90dca066c188ca5480f41
Mustafa BATI <mustafa.bati9@gmail.com>
2022-10-06 21:50:29 +0300

    order details

--- eTrade.Persistence/Services/OrderService.cs
+++ eTrade.Persistence/Services/OrderService.cs
@@ -55,5 +55,29 @@ namespace eTrade.Persistence.Services
                 }).ToListAsync()
             };
         }
+
+        public async Task<SingleOrder> GetOrderByIdAsync(string id)
+        {
+            var data = await _orderReadRepository.Table
+                .Include(o => o.Basket)
+                .ThenInclude(b => b.BasketItems)
+                .ThenInclude(bi => bi.Product)
+                .FirstOrDefaultAsync(o => o.Id == Guid.Parse(id));
+
+            return new()
+            {
+                Id = data.Id.ToString(),
+                BasketItems = data.Basket.BasketItems.Select(bi => new
+                {
+                    bi.Product.Name,
+                    bi.Product.Price,
+                    bi.Quantity
+                }),
+                Address = data.Address,
+                CreatedDate = data.CreatedDate,
+                Description = data.Description,
+                OrderCode = data.OrderCode,
+            };
+        }
     }
 }
