ad39821b4a157844055e52b4a640468adf690aae
Mustafa BATI <mustafa.bati9@gmail.com>
2022-12-03 11:39:28 +0300

    getRoleById and UpdateRole command line

--- Core/eTrade.Application/Abstraction/Services/IRoleService.cs
+++ Core/eTrade.Application/Abstraction/Services/IRoleService.cs
@@ -10,9 +10,9 @@ namespace eTrade.Application.Abstraction.Services
     public interface IRoleService
     {
         (object, int) GetAllRoles(int page, int size);
-        Task<(string id, string name)> GetRoleById(string id);
+        //Task<(string id, string name)> GetRoleById(string id);
         Task<bool> CreateRole(string name);
         Task<bool> DeleteRole(string id);
-        Task<bool> UpdateRole(string id, string name);
+        //Task<bool> UpdateRole(string id, string name);
     }
 }
